8ce81ac951681d31d2b706ca61f5301ec005e0c5
Aizhouym <3551696833@qq.com>
2023-11-18 17:28:20 +0800

    Generate SpringBoot FrameWork

---
+++ PMS-Code/src/main/java/com/example/pmscode/PmsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsCodeApplication.class, args);
+	}
+
+}
---
+++ PMS-Code/src/test/java/com/example/pmscode/PmsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
